de59071068da1d42d525e1a629f43513328609ba
propro7-max <jjuaga97@gmail.com>
2026-05-22 14:19:08 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -114,4 +114,15 @@ app.add_handler(
     )
 )
 
-app.run_polling()
+import asyncio
+
+async def main():
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(3600)
+
+if __name__ == "__main__":
+    asyncio.run(main())
